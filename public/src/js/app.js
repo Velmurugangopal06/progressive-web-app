85b86b4bdfbeb661936638aeceaeb13c0ef9a506
Velmurugan Gopalakrishnan <velmurugangopal06@gmail.com>
2026-07-16 12:39:54 +0530

    (feat) service worker added and configured

---
+++ public/src/js/app.js
@@ -0,0 +1,16 @@
+var deferredPrompt; 
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('../../sw.js')
+        .then(function() {
+            console.log('Service Worker Registered');
+        });
+}
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log('beforeinstallprompt Event fired');
+    event.preventDefault();
+    deferredPrompt = event;
+    return false;
+});
